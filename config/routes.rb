788b62818c047c34d98cb0502003a3cea2706574
Jerard Udemy Course <jerard.r.king@gmail.com>
2017-11-01 08:00:03 +0000

    made home page root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  get 'home', to: 'page#home'
+  root 'page#home'
   get 'about', to: 'page#about'
   
   # You can have the root of your site routed with "root"
